0cfc5cb4c6546c31c70b4efba2061d89a079e605
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-14 18:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function calculatesFarePrice(start, end) {
   if (distance < 400) {
     return 0;
   }
-  else if (400 > distance < 2000) {
+  else if (distance > 400 && distance < 2000) {
     return (distance - 400) * .02;
   }
   else if (distance > 2000 && distance < 2500) {
